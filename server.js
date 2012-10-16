e5af45db7b7e007e1db5a72e25e66664f5937f7d
Hugh Halford-Thompson <hughht5@gmail.com>
2012-10-16 17:23:09 +0100

    server and client stubs now started
    we just need functionality now.

--- server.js
+++ server.js
@@ -1,1 +1,16 @@
 // This file contains the core server logic required to serve the regression run website as well as providing real time updates across clients.
+
+var html = require('fs').readFileSync(__dirname+'/client.html');
+var server = require('http').createServer(function(req, res){
+  res.end(html);
+});
+server.listen(8080);
+
+var nowjs = require("now");
+var everyone = nowjs.initialize(server);
+
+/* Example code to send updates using nowjs
+everyone.now.distributeMessage = function(message){
+  everyone.now.receiveMessage(this.now.name, message);
+};
+//*/
