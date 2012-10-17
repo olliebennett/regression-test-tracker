b858062cef5b115bbffaab608523378e0a9b2f91
Hugh Halford-Thompson <hughht5@gmail.com>
2012-10-17 11:49:45 +0100

    removing express support
    Decided it got too complex. not required for this app.

--- server.js
+++ server.js
@@ -1,29 +1,13 @@
 // This file contains the core server logic required to serve the regression run website as well as providing real time updates across clients.
-var express = require('express'),
-	fs = require('fs');
-	
-var indexPage = fs.readFileSync(__dirname+'/public/client.html');
-	
+var path = require('path'),
+	html = require('fs').readFileSync(__dirname+'/public/client.html');
 	
 var server = require('http').createServer(function(req, res){
-
-	console.log('Starting server on port ' + options.port);
-	
-	var server = express.createServer(
-            express.static('/public')),
-			express.staticCache(),
-            express.router(function(app) {
-				app.get('/*', function(req, res, next) {
-					res.end(indexPage);
-				});
-			})
-        ).listen(options.port);
-	
 	if (req === "sadf"){
 		console.log("asdf");
 	}else{
 		console.log("unknown request - serving index page");
-		res.end(indexPage);
+		res.end(html);
 	}
 });
 server.listen(8080);
